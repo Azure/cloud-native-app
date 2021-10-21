8b01d66e88d9a24974ee2f400a1b741cc2c0096d
seenu433 <seenu433@gmail.com>
2021-10-21 10:46:52 -0400

    Update setup.sh

--- gitops/setup.sh
+++ gitops/setup.sh
@@ -4,7 +4,7 @@ echo "## Installing Flux"
 curl -s https://fluxcd.io/install.sh | sudo bash
 
 echo "## Installing Kube Seal"
-wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.16.0/kubeseal-linux-amd64 -O kubeseal
+sudo wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.16.0/kubeseal-linux-amd64 -O kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal && rm kubeseal
 
 echo "##Flux Bootstap with kubeseal"
@@ -21,30 +21,30 @@ sudo dpkg -i step-cli_0.15.2_amd64.deb
 cd cloud-native-app/gitops/infrastructure/linkerd
 
 echo "## Generate Certificates for linkerd"
-step certificate create identity.linkerd.cluster.local ca.crt ca.key \
+sudo step certificate create identity.linkerd.cluster.local ca.crt ca.key \
 	--profile root-ca --no-password --insecure \
 	--san identity.linkerd.cluster.local
 
-step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
+sudo step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
 	--ca ca.crt --ca-key ca.key --profile intermediate-ca --not-after 8760h --no-password --insecure \
 	--san identity.linkerd.cluster.local
 
 echo "## Generate Secrets from Certs"
-kubectl -n linkerd create secret generic certs \
+sudo sh -c "kubectl -n linkerd create secret generic certs \
 	--from-file=ca.crt --from-file=issuer.crt \
 	--from-file=issuer.key -oyaml --dry-run=client \
-	> certs.yaml
+	> certs.yaml"
 
 echo "## Download kube seal publick key"
-sleep 1m; kubeseal --fetch-cert \
+sudo sh -c "sleep 1m; kubeseal --fetch-cert \
 --controller-name=sealed-secrets-controller \
 --controller-namespace=flux-system \
-> ../../../../pub-sealed-secrets.pem
+> ../../../../pub-sealed-secrets.pem"
 
 echo "# Seal Certs Secrets"
-kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
-< certs.yaml > certs-sealed.yaml
-rm certs.yaml
+sudo sh -c "kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
+< certs.yaml > certs-sealed.yaml"
+sudo rm certs.yaml
 
 cd ../../..
 
@@ -54,52 +54,52 @@ registryHostDnsLabel=`echo $registryHost | cut -d '.' -f 1`
 
 echo "## Replace tokens in yamls"
 exp=$(date -d '+8760 hour' +"%Y-%m-%dT%H:%M:%SZ")
-sed -i "s/{cert_expiry}/$exp/g" gitops/clusters/production/infrastructure-linkerd.yaml
-sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/infrastructure-harbor.yaml
-sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-harbor.yaml
-sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-seed.yaml
-sed -i "s/{cluster_issuer_email}/$cluster_issuer_email/g" gitops/clusters/production/infrastructure-certmanager.yaml
+sudo sed -i "s/{cert_expiry}/$exp/g" gitops/clusters/production/infrastructure-linkerd.yaml
+sudo sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/infrastructure-harbor.yaml
+sudo sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-harbor.yaml
+sudo sed -i "s%{registryUrl}%$registryUrl%g" gitops/clusters/production/infrastructure-seed.yaml
+sudo sed -i "s/{cluster_issuer_email}/$cluster_issuer_email/g" gitops/clusters/production/infrastructure-certmanager.yaml
 
-sed -i "s/{cicdWebhookHost}/$appHostName/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{appHostName}/$appHostName/g" gitops/clusters/production/app-devops.yaml
-sed -i "s/{sendGridApiKey}/$sendGridApiKey/g" gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{cicdWebhookHost}/$appHostName/g" gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{registryHost}/$registryHost/g" gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{appHostName}/$appHostName/g" gitops/clusters/production/app-devops.yaml
+sudo sed -i "s/{sendGridApiKey}/$sendGridApiKey/g" gitops/clusters/production/app-devops.yaml
 
-sed -i "s/{registryHostDnsLabel}/$registryHostDnsLabel/g" gitops/clusters/production/infrastructure-harbor-nginx.yaml
-sed -i "s/{appHostDnsLabel}/$appHostDnsLabel/g" gitops/clusters/production/infrastructure-nginx.yaml
+sudo sed -i "s/{registryHostDnsLabel}/$registryHostDnsLabel/g" gitops/clusters/production/infrastructure-harbor-nginx.yaml
+sudo sed -i "s/{appHostDnsLabel}/$appHostDnsLabel/g" gitops/clusters/production/infrastructure-nginx.yaml
 
 cd gitops/app/core
 
 echo "## Generate registry secrets for conexp namespace"
-kubectl create secret docker-registry regcred \
+sudo sh -c "kubectl create secret docker-registry regcred \
 	--docker-server="https://$registryHost" --docker-username=conexp  --docker-password=FTA@CNCF0n@zure3  --docker-email=user@mycompany.com -n conexp-mvp -oyaml --dry-run=client \
-	> regcred-conexp.yaml
+	> regcred-conexp.yaml"
 
 echo "## Seal the secret"
-kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
-< regcred-conexp.yaml > regcred-conexp-sealed.yaml
-rm regcred-conexp.yaml
+sudo sh -c "kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
+< regcred-conexp.yaml > regcred-conexp-sealed.yaml"
+sudo rm regcred-conexp.yaml
 
 echo "## Generate registry secrets for openfaas namespace"
-kubectl create secret docker-registry regcred \
+sudo sh -c "kubectl create secret docker-registry regcred \
 	--docker-server="https://$registryHost" --docker-username=conexp  --docker-password=FTA@CNCF0n@zure3  --docker-email=user@mycompany.com -n openfaas-fn -oyaml --dry-run=client \
-	> regcred-openfaas.yaml
+	> regcred-openfaas.yaml"
 
 echo "## Seal the secret"
-kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
-< regcred-openfaas.yaml > regcred-openfaas-sealed.yaml
-rm regcred-openfaas.yaml
+sudo sh -c "kubeseal --format=yaml --cert=../../../../pub-sealed-secrets.pem \
+< regcred-openfaas.yaml > regcred-openfaas-sealed.yaml"
+sudo rm regcred-openfaas.yaml
 
 cd ../../..
 
 echo "## Commit the changes to the git repo"
-git remote set-url origin "https://$owner:$GITHUB_TOKEN@github.com/$owner/cloud-native-app.git"
-git config user.email "$cluster_issuer_email"
-git config user.name "Auto"
-git pull
-git add *
-git commit -m  "Auto commit prep files"
-git push -u origin main
+sudo git remote set-url origin "https://$owner:$GITHUB_TOKEN@github.com/$owner/cloud-native-app.git"
+sudo git config user.email "$cluster_issuer_email"
+sudo git config user.name "Auto"
+sudo git pull
+sudo git add *
+sudo git commit -m  "Auto commit prep files"
+sudo git push -u origin main
 
 echo "## Add Kustomization to reconcile the app"
 flux create kustomization cloud-native-app \
